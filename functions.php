888db12144ac7cf94168a4e02f118fad0b6ba867
mia-isa <113313205+mia-isa@users.noreply.github.com>
2023-05-09 10:55:30 -0700

    functions.php 20230509
    working as of 20230509

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php session_start(); 
+
+$nav = '<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="/home.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/userProfile/homeProfile.php">Your profile</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/browse/browseHome.php">Browse posts</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/userSession/logout.php">Log out</a>
+  </li>
+</ul>';
+  
+?>
