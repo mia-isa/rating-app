138c59c2649e2af7f91888dcd0bae6084c8a3ce9
mia-isa <113313205+mia-isa@users.noreply.github.com>
2023-05-21 13:12:49 -0700

    functions.php 20230521
    fixed nav up a bit, and added json :)

--- functions.php
+++ functions.php
@@ -1,21 +1,24 @@
 <?php session_start(); 
 
+  $itemsJSON = file_get_contents("https://php-always-on.toddbenrud.repl.co/webDesign/reviewCRUD/itemData/item.json");
+  $itemsArray =json_decode($itemsJSON, TRUE);
+
 $nav = '<ul class="nav nav-tabs">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="/home.php">Home</a>
+    <a class="nav-link" aria-current="page" href="/home.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" aria-current="page" href="/index.php">Login</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="/userProfile/homeProfile.php">Your profile</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="/browse/browseHome.php">Browse posts</a>
-  </li>
-  <li class="nav-item">
     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="/userSession/logout.php">Log out</a>
+    <a class="nav-link" href="/accessPoint.php">Log out</a>
   </li>
 </ul>';
-  
+
 ?>
