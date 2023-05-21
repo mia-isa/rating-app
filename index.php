0a2d05a668acf56be33df64338982e4813c17def
mia-isa <113313205+mia-isa@users.noreply.github.com>
2023-05-21 13:10:45 -0700

    index.php 20230521
    it finally works!!! it looks janky but that's okay!! it's all good now :)

--- index.php
+++ index.php
@@ -1,77 +1,75 @@
-<?php include ($_SERVER['DOCUMENT_ROOT'].'/functions.php');
-$jsonCats = file_get_contents("https://php-always-on.toddbenrud.repl.co/webDesign/reviewCRUD/itemData/item.json");
-$catData = json_decode($jsonCats, TRUE); 
-?>
-<!DOCTYPE html>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/functions.php'); ?>
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="author" content="Muhamad Nauval Azhar">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta name="description" content="This is a login page template based on Bootstrap 5">
-	<title>RavingVGs | Login</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-  <link href='/style.css' name="stylesheet">
-</head>
-  <nav>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="/style.css" name="stylesheet">
+
+<!-- nav -->
     <?php echo $nav; ?>
-  </nav>
+  </head>
 
-<body>
-	<section class="h-100">
-		<div class="container h-100">
-			<div class="row justify-content-sm-center h-100">
-				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-					<div class="text-center my-5">
-					</div>
-					<div class="card shadow-lg">
-						<div class="card-body p-5">
-							<h1 class="fs-4 card-title fw-bold mb-4">Welcome back!</h1>
-              
-<form action="https://php-always-on.toddbenrud.repl.co/webDesign/reviewCRUD/accessPoint.php" method="post" class="needs-validation" novalidate="" autocomplete="off" name="login">
-	<div class="mb-3">
-		<label class="mb-2 text-muted" for="email">E-Mail Address</label>
-			<input id="email" type="email" class="form-control" name="email" value="" required autofocus>
-		<div class="invalid-feedback">
-			Email is invalid
-	 </div>
+    <title>Hello, world!</title>
+  </head>
+
+  <body>
+  <div class="container">
+  <div class="row">
+    <div class="col mt-2">
+      <h2>Log in</h2>
+    </div>
   </div>
-<div class="mb-3">
-	<div class="mb-2 w-100">
-		<label class="text-muted" for="password">Password</label>
-		<a href="/userSession/forgot.php" class="float-end">
-			Forgot Password?
-		</a>
-	</div>
-  <input id="url" type="hidden" class="form-control" name="url" value="https://web-final-crud-webdesignghs2022-1.webdesignghs2022.repl.co/index.php" required>
-		<div class="invalid-feedback">
-			Email is invalid
-	 </div>
-<input id="password" type="password" class="form-control" name="pword" required>
-	<div class="invalid-feedback">
-		Password is required
-	 </div>
-	</div>
+</div>
 
-	 <div class="d-flex align-items-center">
-	 <input type="submit" value="Submit" name="BTN_login">
+  <div class="row m-2">
+    <div class="col m-2">
+<form action="https://php-always-on.toddbenrud.repl.co/webDesign/reviewCRUD/accessPoint.php" method="post" autocomplete="off" name="login"><br>
+   <input id="url" type="hidden" class="form-control" name="url" value="https://web-final-crud-webdesignghs2022-1.webdesignghs2022.repl.co/index.php" required>
+  <label for="email">Email</label><br>
+			<input type="email" class="form-control" name="email" value=""><br>
+  <label class="text-muted" for="password">Password</label><BR>
+      <input type="password" class="form-control" name="pword" required><BR>
+<input type="submit" value="Submit" name="BTN_login">
+</form> 
+    </div>
   </div>
-</form>
-				</div>
-						<div class="card-footer py-3 border-0">
-							<div class="text-center">
-								Don't have an account? <a href="/userSession/register.php" class="text-dark">Create One</a>
-							</div>
-						</div>
-					</div>
-					<div class="text-center mt-5 text-muted">
-						Welcome (or welcome back) to RavingVGs! We hope you have a wonderful time :)
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
 
-	<script src="/script.js"></script>
-</body>
+    <div class="row m-2">
+    <div class="col m-2">
+      <?php 
+echo '<pre>'; 
+var_dump($_GET);
+echo '</pre>'; 
+
+function verifyLogin() {
+      $loggedIn = 'no';
+      if (isset($_SESSION['status']['uid'])) {
+        if (is_numeric($_SESSION['status']['uid']) && strlen($_SESSION['status']['uid']) == 3) {
+            $loggedIn = 'yes';
+        } // END:: if() numeric and 3 chars
+      }// END:: if() isset
+  
+      if ($loggedIn == 'no'){
+        session_unset();
+        die();
+      }// END:: if() redirect
+    
+  } // END:: FUNCTION
+
+verifyLogin();
+      ?>
+    </div>
+    </div>
+
+  </body>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="/script.js" name="js">
+   
+  </body>
 </html>
